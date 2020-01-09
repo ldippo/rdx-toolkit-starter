0bfa005962736af9c5f002732851cbe8907c0dbe
Lyle Dippo <ldippo421@gmail.com>
2020-01-09 10:12:04 -0500

    remove unused bp, add store utilities

--- src/StateBlock.tsx
+++ src/StateBlock.tsx
@@ -1,23 +1,31 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { fetchPost } from "./core/store/post";
+import { useTypedSelector } from "./core/store/util";
 
 export function StateBlock() {
   const dispatch = useDispatch();
-  const state = useSelector(state => state);
+  const postState = useTypedSelector(state => state.posts);
   const [inputVal, setInputVal] = React.useState(1);
+
+  const onInputChange = React.useCallback(
+    e => setInputVal(Number(e.currentTarget.value)),
+    []
+  );
+
+  const onButtonClick = React.useCallback(() => dispatch(fetchPost(inputVal)), [
+    inputVal,
+    dispatch
+  ]);
+
   return (
     <>
-      <input
-        type="number"
-        value={inputVal}
-        onChange={e => setInputVal(Number(e.currentTarget.value))}
-      />
+      <input type="number" value={inputVal} onChange={onInputChange} />
       <button
-        onClick={() => dispatch(fetchPost(inputVal))}
+        onClick={onButtonClick}
         children={`get post with id ${inputVal}`}
       />
-      <pre>{JSON.stringify(state, null, 4)}</pre>
+      <pre>{JSON.stringify(postState, null, 4)}</pre>
     </>
   );
 }
